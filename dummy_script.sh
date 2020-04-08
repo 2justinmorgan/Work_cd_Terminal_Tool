917febe41b2be885330d67b74966424de4ec7a8f
Justin_Morgan <2justinmorgan@gmail.com>
2020-04-07 17:08:14 -0700

    adding dummy_script.sh

---
+++ dummy_script.sh
@@ -0,0 +1,5 @@
+# This is a dummy Bash script so GitHub displays "Bash" as the primary
+# language of this repo. 
+
+# The "work" script/utility uses the user's environment, instead of creating
+# a new environment via the #!/bin/bash interpreter
