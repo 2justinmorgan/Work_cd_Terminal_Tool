cf616a755f8ba12a2b909f329dbe95a0db7eb98e
Justin_Morgan <2justinmorgan@gmail.com>
2020-04-07 17:17:34 -0700

    just added sh file extension

--- dummy_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This is a dummy Bash script so GitHub displays "Bash" as the primary
-# language of this repo. 
-
-# The "work" script/utility uses the user's environment, instead of creating
-# a new environment via the #!/bin/bash interpreter
